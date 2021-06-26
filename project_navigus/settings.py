d5fa4e5d39a66ddd7c3ae255ad9efc3f12aefc73
AbhyudayaBora <codeluna99@gmail.com>
2021-06-26 15:46:35 +0530

    Added deploymentfile and requirements for heroku

--- project_navigus/settings.py
+++ project_navigus/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -145,3 +146,6 @@ EMAIL_HOST_PASSWORD = 'xyz' # host email password required
 # otherwise you will get SMTPAuthenticationError at /contactus
 # this process is required because google blocks apps authentication by default
 EMAIL_RECEIVING_USER = ['to@gmail.com'] # email on which you will receive messages sent from website
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
